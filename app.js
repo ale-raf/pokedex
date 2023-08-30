9bdff37f8527333dfb60dd8df6cdf2357b7d5132
Ale Raf <raffinalexis@gmail.com>
2023-08-30 18:32:14 +0200

    functions

--- app.js
+++ app.js
@@ -1,22 +1,57 @@
-const pokemonName = document.querySelector(".title-card");
-const pokemonImg = document.querySelector(".image");
-const talentsList = document.querySelector(".talents");
-fetch("https://api-pokemon-fr.vercel.app/api/v1/pokemon/tortank").then(
-  (res) => {
+const container = document.querySelector(".pokedex-container");
+const search = document.getElementById("search");
+let pokedex;
+
+function catchAllPokemons() {
+  fetch("https://api-pokemon-fr.vercel.app/api/v1/pokemon").then((res) => {
     if (res.ok) {
-      res.json().then((data) => {
-        pokemonName.innerHTML = data.name.fr;
-        pokemonImg.src = data.sprites.regular;
-        pokemonImg.alt = data.name.fr;
-        console.log(document.querySelector(".image"));
-        data.talents.filter((talent) => {
-          const talentName = document.createElement("li");
-          talentName.innerHTML = talent.name;
-          talentsList.appendChild(talentName);
-        });
-      });
+      res.json().then(createPokemonCard);
     } else {
       console.log("error");
     }
+  });
+}
+
+function createPokemonCard(pokedex) {
+  const randomPokedex = pokedex.sort((a, b) => 0.5 - Math.random());
+  for (let pokemon of randomPokedex) {
+    if (pokemon.pokedexId > 0) {
+      const pokemonCard = document.createElement("article");
+      const pokemonName = document.createElement("h3");
+      const pokemonImg = document.createElement("img");
+
+      pokemonCard.classList.add("card-wrapper");
+      pokemonName.innerHTML = pokemon.name.fr;
+      pokemonName.classList.add("title-card");
+      pokemonImg.src = pokemon.sprites.regular;
+      pokemonImg.alt = `image de ${pokemon.name.fr}`;
+      container.appendChild(pokemonCard);
+      pokemonCard.append(pokemonName, pokemonImg);
+
+      showPokemonTalents(pokemon, pokemonCard);
+    }
   }
-);
+}
+
+function showPokemonTalents(pokemon, card) {
+  pokemon.talents.filter((talent) => {
+    const talentsList = document.createElement("ul");
+    const talentName = document.createElement("li");
+    talentName.innerHTML = talent.name;
+    talentsList.classList.add("talents");
+    card.appendChild(talentsList);
+    talentsList.appendChild(talentName);
+  });
+}
+
+search.addEventListener("keyup", catchSomePokemons);
+// pokedex = ["Pikachu", "Yuyu"];
+function catchSomePokemons(e) {
+  const value = e.target.value.toLowerCase();
+  const filter = pokedex.filter((pokemon) =>
+    pokemon.toLowerCase().includes(value)
+  );
+  console.log(filter);
+}
+
+catchAllPokemons();
